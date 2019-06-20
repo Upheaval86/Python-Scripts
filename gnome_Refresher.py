a702f7de3d01215d05099fc01e9923f701ea30cf
Ashley W <Upheaval86@users.noreply.github.com>
2019-06-20 05:30:30 -0600

    Create gnome_Refresher.py

---
+++ gnome_Refresher.py
@@ -0,0 +1,17 @@
+# Version 0.1 by Ashley Watson
+# Used to automate restarting the GNOME desktop
+
+import sys
+import time
+import easygui
+from pykeyboard import PyKeyboard
+
+k = PyKeyboard()
+
+easygui.msgbox("This will refresh the GNOME Desktop. Click OK to continue..", title="GNOME Refresher", ok_button="OK")
+k.press_keys([k.alt_key, k.function_keys[2]])
+time.sleep(1)
+k.tap_key('r')
+k.tap_key(k.enter_key)
+time.sleep(5)
+easygui.msgbox("GNOME has been refreshed. Click below to close.", title="GNOME Refresher", ok_button="Close")
