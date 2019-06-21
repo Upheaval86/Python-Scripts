4d5578ddae9d61635c84d8695517188576b31c18
Ashley W <Upheaval86@users.noreply.github.com>
2019-06-20 20:03:56 -0600

    Update gnome_Refresher.py
    Updated UI with Cancel prompt alongside Continue option

--- gnome_Refresher.py
+++ gnome_Refresher.py
@@ -1,5 +1,9 @@
-# Version 0.1 by Ashley W.
+# Version 0.2 by Ashley Watson
 # Used to automate restarting the GNOME desktop
+#
+# Changelog
+# V0.1 - Initial release
+# V0.2 - Graphical prompt to confirm user action
 
 import sys
 import time
@@ -8,7 +12,13 @@ from pykeyboard import PyKeyboard
 
 k = PyKeyboard()
 
-easygui.msgbox("This will refresh the GNOME Desktop. Click OK to continue..", title="GNOME Refresher", ok_button="OK")
+msg = "This will refresh the GNOME Desktop. Click OK to continue or Cancel to close without refreshing"
+title = "GNOME Refresher"
+if easygui.ccbox(msg, title):   # shows a Continue / Cancel box
+    pass    # user chose Continue
+else:   # user chose Cancel
+    sys.exit(0)
+
 k.press_keys([k.alt_key, k.function_keys[2]])
 time.sleep(1)
 k.tap_key('r')
